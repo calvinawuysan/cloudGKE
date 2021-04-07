b9693d140a1e8d1adb3230dac3dd5ae3b8bd56d4
calvinawuysan <71801047+calvinawuysan@users.noreply.github.com>
2021-04-07 11:10:57 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send('Calvina Wuysan\n')
+    res.send('51018003\n')
+    res.send('Sistem Informasi 2018\n')
 })
 
 app.listen(PORT, HOST)
